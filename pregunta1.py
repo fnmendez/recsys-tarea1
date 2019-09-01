554e9b4e9f15312f1fc0d399a2b0f9018b02b00d
wayoalamos <malamos2@uc.cl>
2019-09-01 13:54:33 -0400

    .

--- pregunta1.py
+++ pregunta1.py
@@ -7,17 +7,31 @@ import scipy.sparse as sparse
 # import implicit
 # import scipy.sparse as sparse
 
-df_train = pd.read_csv('files/training.csv',
-                        sep=",",
-                        )
+df_train = pd.read_csv('files/training.csv')
 
-print(df_train.head())
-print(df_train.columns)
+# usuarios y cantidad de imagenes que vieron
+photos_by_user = df_train.groupby(['user']).count()[['image_id']].reset_index()
 
-photos_by_user = df_train.groupby(['user']).count()[['image_id']]
-# se le deberia cambiar el nombre a la columna
-print(photos_by_user)
-print(photos_by_user.columns)
+# ordenar y ver los 5 que vieron los mas grandes
+top_five = photos_by_user.sort_values(by=['image_id'], ascending=False).iloc[:5]
+print(top_five)
 
 
-#print(photos_by_user)
+
+# cantidad de usuarios y cantidad de interacciones
+p = photos_by_user.groupby(['image_id']).count()[['user']].reset_index()
+p.columns = ['times_rated', 'amount_users']
+print(p.head())
+
+
+
+
+
+# imagen y la cantidad de usuarios que la vieron
+users_by_photo = df_train.groupby(['image_id']).count()[['user']].reset_index()
+
+# ordenar y ver las 5 imagenes mas vistas
+image_top_five = users_by_photo.sort_values(by=['user'], ascending=False).iloc[:5]
+
+
+print(image_top_five.head())
