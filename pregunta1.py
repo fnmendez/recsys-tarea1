784b161d740ba5fedb7e99f16d7467fc34d020d3
wayoalamos <malamos2@uc.cl>
2019-08-31 20:35:55 -0400

    avance 1 martin

--- pregunta1.py
+++ pregunta1.py
@@ -8,10 +8,16 @@ import scipy.sparse as sparse
 # import scipy.sparse as sparse
 
 df_train = pd.read_csv('files/training.csv',
-                        sep="\t",
-                        header=0,
+                        sep=",",
                         )
+
 print(df_train.head())
+print(df_train.columns)
+
+photos_by_user = df_train.groupby(['user']).count()[['image_id']]
+# se le deberia cambiar el nombre a la columna
+print(photos_by_user)
+print(photos_by_user.columns)
 
 
-#df.groupby(['userid']).count()
+#print(photos_by_user)
