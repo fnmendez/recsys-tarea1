e4e5edda891c591d21295f563fc3a14c3802cac0
Franco Méndez <fnmendez@uc.cl>
2019-09-03 19:49:37 -0400

    add separator

---
+++ prepare_dataset.py
@@ -0,0 +1,28 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+train_frac = .8
+
+df_train = pd.read_csv('files/training.csv')
+
+df_per_user = df_train.groupby(['user']).count()[['image_id']].reset_index()
+df_per_user.columns = ['user_id', 'visits']
+
+def separate(data):
+    print('Separating data...')
+    users_ids = pd.unique(data['user'])
+    train_set = pd.DataFrame(None, columns = ['user', 'image_id', 'timestamp'])
+    test_set = pd.DataFrame(None, columns = ['user', 'image_id', 'timestamp'])
+
+    for id in users_ids:
+        visit_count = int(df_per_user[df_per_user['user_id'] == id]['visits'])
+        if visit_count < 2:
+            continue
+        n_train = int(visit_count * train_frac)
+        train_set = pd.concat([train_set, data[data['user'] == id][:n_train]])
+        test_set = pd.concat([test_set, data[data['user'] == id][n_train:]])
+
+    return train_set, test_set
+
+train, test = separate(df_train)
