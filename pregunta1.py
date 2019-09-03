d1f11a555833a007d774c24f4a55b366787bfbff
wayoalamos <malamos2@uc.cl>
2019-09-03 14:47:05 -0400

    ,

--- pregunta1.py
+++ pregunta1.py
@@ -8,6 +8,7 @@ import scipy.sparse as sparse
 # import scipy.sparse as sparse
 
 views = {} # user : numer_of_interactions
+image_views = {} # image: number_of_interactions
 
 fo = open("files/training.csv", "r")
 fo.readline()
@@ -17,38 +18,101 @@ for line in fo.readlines():
         views[user] += 1
     else:
         views[user] = 1
+
+    if image in image_views:
+        image_views[image] +=1
+    else:
+        image_views[image] = 1
 fo.close()
 
-user_interaction = {} # number_of_interactions : [user1, user2, ... ]
-
+user_interaction = {} # number_of_interactions : amount_of_users
 for user in views:
     if views[user] in user_interaction:
-        user_interaction[views[user]].append(user)
+        user_interaction[views[user]] += 1
     else:
-        user_interaction[views[user]] = [user]
+        user_interaction[views[user]] = 1
 
-# get top 5
-top_five = [] # user1, user2, ...
+image_interaction = {} # number_of_interactions : amount_of_images
+for image in image_views:
+    if image_views[image] in image_interaction:
+        image_interaction[image_views[image]] += 1
+    else:
+        image_interaction[image_views[image]] = 1
+
+# get top
+top_five_users = [] # user1, user2, ...
 counter = 0
 for key, value in sorted(views.items(), key=lambda kv: kv[1], reverse=True):
-    top_five.append(key)
+    top_five_users.append(key)
     counter += 1
     if counter > 4: break
 
+top_five_image = [] # user1, user2, ...
+counter = 0
+for key, value in sorted(image_views.items(), key=lambda kv: kv[1], reverse=True):
+    top_five_image.append(key)
+    counter += 1
+    if counter > 4: break
+# finish get top 5
+
 # lets graph!
-graph_data = [] # (number_of_interactions, number_of_users, percentage)
+user_graph_data = [] # (number_of_interactions, number_of_users rango de 3)
 for k in user_interaction:
-    tops_counter = 0
-    total_amount_users = len(user_interaction[k])
-    for user in user_interaction[k]:
-        if user in top_five:
-            tops_counter += 1
-    percentage = 100 * float(tops_counter) / float(total_amount_users)
-    # print(tops_counter, total_amount_users, user_interaction[k], top_five)
-    graph_data.append((k, total_amount_users, percentage))
-graph_data = sorted(graph_data)
+    user_graph_data.append((k, user_interaction[k]))
+user_graph_data = sorted(user_graph_data)
+
+image_graph_data = []
+for k in image_interaction:
+    image_graph_data.append((k, image_interaction[k]))
+image_graph_data = sorted(image_graph_data)
+
+# aca agrupamos la info para que no sea tan grande
+new_graph_data = [] # ('0-3', 74), ('4-7', 95)
+x_name = ''
+count = 0
+counter = 0
+for x, y in user_graph_data:
+    counter += y
+    if count == 0:
+        x_name = str(x) + '-'
+    if count == 5:
+        x_name += str(x)
+        new_graph_data.append((x_name, counter))
+        count = -1
+        counter = 0
+    count += 1
+
+plt.bar([i[0] for i in new_graph_data], [j[1] for j in new_graph_data])
+plt.xlabel('Numero de interacciones', fontsize=15)
+plt.ylabel('Numero de usuarios', fontsize=15)
+plt.xticks([i[0] for i in new_graph_data], [i[0] for i in new_graph_data], rotation=70)
+#plt.show()
+print("los 5 usuarios mas activos son :", top_five_users)
+
+total_interactions = 0
+interactions_made_by_top_5_users = 0
+interactions_in_top_5_images = 0
+
+for user in views:
+    total_interactions += views[user]
+    if user in top_five_users:
+        interactions_made_by_top_5_users += views[user]
+print("total_interactions: ", total_interactions)
+print("made by top 5 users", interactions_made_by_top_5_users)
+percent_user = 100 * interactions_made_by_top_5_users / total_interactions
+print("percent made by top 5 users", percent_user)
+
+for image in image_views:
+    if image in top_five_image:
+        interactions_in_top_5_images += image_views[image]
+percent_imaage = 100 * interactions_in_top_5_images / total_interactions
+print("percent of image", percent_imaage)
+
+print("made by in top 5 images", interactions_in_top_5_images)
 
 
-print(graph_data)
-plt.bar([i[0] for i in graph_data],[j[1] for j in graph_data])
-plt.show()
+plt.xlabel('Numero de interacciones', fontsize=15)
+plt.ylabel('Numero de imagenes', fontsize=15)
+plt.bar([str(i[0]) for i in image_graph_data], [j[1] for j in image_graph_data])
+#plt.show()
+print("los 5 imagenes mas vistas son :", top_five_image)
