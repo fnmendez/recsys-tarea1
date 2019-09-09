293ced6fd0240253808d6a35318103b56689d539
Franco Méndez <fnmendez@uc.cl>
2019-09-09 16:14:44 -0300

    use pandas

--- pregunta3.py
+++ pregunta3.py
@@ -1,10 +1,10 @@
-import json
+import pandas as pd
 from sklearn.decomposition import PCA
 
 EMBEDDINGS_FILE = 'files/image_embeddings.json'
 
 with open(EMBEDDINGS_FILE) as json_data:
-    _X = json.load(json_data)
+    _X = pd.read_json(json_data)
 
 pca = PCA(n_components=3)
 pca.fit(_X)
